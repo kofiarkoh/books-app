e34a48719ef16804db1fa8df928f13a92b9ae1d8
lawrence <kofiarkoh0@gmail.com>
2023-08-02 22:12:54 +0000

    add set up for redux toolkit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
 "use client";
-import "./globals.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import {ThemeProvider, createTheme} from "@mui/material/styles";
-
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import "./globals.css";
+import {Provider} from "react-redux";
+import {reduxStore} from "../redux/store";
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
 		<html lang="en">
-			<ThemeProvider theme={createTheme()}>
-				<body>{children}</body>
-			</ThemeProvider>
+			<Provider store={reduxStore}>
+				<ThemeProvider theme={createTheme()}>
+					<body>{children}</body>
+				</ThemeProvider>
+			</Provider>
 		</html>
 	);
 }
