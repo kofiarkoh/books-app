d55643ade90d8e4d003620bd0695cdbe1ab858f4
lawrence <kofiarkoh0@gmail.com>
2023-08-04 21:39:48 +0000

    add logout handler

--- src/app/books/layout.tsx
+++ src/app/books/layout.tsx
@@ -1,11 +1,30 @@
+"use client";
 import LogoutIcon from "@mui/icons-material/Logout";
 import AppBar from "@mui/material/AppBar";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import React from "react";
+import React, {useState} from "react";
+import ConfirmDialog from "@/components/ConfirmDialog";
+import {useRouter} from "next/navigation";
+import {routes} from "../../routes";
 
 export default function Layout({children}: {children: React.ReactNode}) {
+	const [isDialogVisible, setDialogVisible] = useState(false);
+	const router = useRouter();
+
+	const logOut = () => {
+		sessionStorage.clear();
+		router.push(routes.login);
+	};
+
+	const closeDialog = () => {
+		setDialogVisible(false);
+	};
+
+	const openDialog = () => {
+		setDialogVisible(true);
+	};
 	return (
 		<div>
 			<AppBar position="fixed" sx={{position: "fixed"}}>
@@ -17,13 +36,22 @@ export default function Layout({children}: {children: React.ReactNode}) {
 						size="large"
 						edge="start"
 						color="inherit"
-						aria-label="menu"
+						aria-label="log out"
+						onClick={openDialog}
 						sx={{mr: 2}}>
 						<LogoutIcon />
 					</IconButton>
 				</Toolbar>
 			</AppBar>
 			<div style={{paddingTop: "40px"}}>{children}</div>
+			<ConfirmDialog
+				onAccept={logOut}
+				onCancel={closeDialog}
+				title={"Do you want log out?"}
+				message={"You will need to login again to view your books"}
+				visible={isDialogVisible}
+				isloading={false}
+			/>
 		</div>
 	);
 }
